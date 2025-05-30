2bca337959914fbc81f35a113fd5122cb88bd6ab
Olúwafẹmi Love <42425031+oluwafemi-love@users.noreply.github.com>
2025-05-31 01:47:46 +0200

    Solved

--- src/script.js
+++ src/script.js
@@ -1,30 +1,79 @@
 // Definera array av namn
-let people;
+let people = [
+  "Femi",
+  "Tobi",
+  "Jael",
+  "Bori",
+  "Chinedu",
+  "Aisha",
+  "Ngozi",
+  "Temi",
+  "Emeka",
+  "Zainab",
+  "Ayodele",
+  "Kelechi",
+  "Ifeoma",
+  "Sani",
+  "Bolaji",
+  "Nneka",
+  "Yusuf",
+  "Funmi",
+  "Obinna",
+  "Hadiza",
+  "Lisa"
+];
 
-
-export function initSearch(people) { // rör ej denna rad
+export function initSearch(people) {
+  // rör ej denna rad
 
   // Hämta HTML-element
-  
+  let searchInput = document.getElementById("searchInput");
+  let searchBtn = document.getElementById("searchBtn");
+  let resultDisplay = document.getElementById("resultDisplay");
+
   // Lägg till en klickhändelse på "Sök"-knappen
-  searchBtn.addEventListener("click",() => {
-  
+  searchBtn.addEventListener("click", () => {
     // 1. Hämta texten i searchInput.value
-    
+    let searchQuery = searchInput.value.trim().toLowerCase();
+
     // 2. Skapa en boolean för found
-    
+    let foundSearchQuery = false;
+
+    //This code Works:
+    // for (let i = 0; i < people.length; i++) {
+
+    //   if (searchQuery === people[i]) {
+    //     resultDisplay.innerText = "Namn hittades!";
+    //     foundSearchQuery = true;
+    //     break;
+    //   } else if(searchQuery !== people[i]) {
+    //     resultDisplay.innerText = "Namn hittades inte.";
+    //     foundSearchQuery = false;
+    //   }
+    // }
+
     // 3. Loopa igenom people-arrayen
-    
+    for (const prsnName of people){
+      if(searchQuery === prsnName.toLowerCase()){
+        foundSearchQuery = true;
+        break;
+      }
+    }
+
+    if(foundSearchQuery){
+      resultDisplay.innerText = "Namn hittades!"
+    }else{
+      resultDisplay.innerText = "Namn hittades inte.";
+    }
+
+
     // 4. Jämför texten med varje namn i arrayen
-    
+
     // 5. Om du hittar en match, skriv ut "Namn hittades!" i resultDisplay, ändra found till true
-    
+
     // 6. Om du efter loopen inte hittat något, skriv ut "Namn hittades inte." och gör found till false igen.
-    
-    });
+  });
 }
-
-
 
 // rör ej dessa rader
 document.addEventListener("DOMContentLoaded", () => {
